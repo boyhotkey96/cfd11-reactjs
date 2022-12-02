7617f85f38d942107a6921a73ca198dc118fcd63
boyhotkey96 <boyhotkey96@gmail.com>
2022-12-02 14:20:04 +0700

    update: userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,19 +1,28 @@
 import api from "../config/api"
 
 const userService = {
-    updateInfo(data) {
-        return api.post('/user/update', { data })
-    },
-    getInfo() {
-        const token = JSON.parse(localStorage.getItem('token'))
-        if (token) {
-            return api.get('/user/get-info', {
-                headers: {
-                    Authorization: `Bearer ${token.accessToken}`
-                }
-            })
-        }
+  updateInfo(data) {
+    const token = JSON.parse(localStorage.getItem('token'))
+    if (token) {
+      return api.get('/user/update', {
+        headers: {
+          Authorization: `Bearer ${token.accessToken}`
+        },
+        data,
+      })
     }
+    // return api.post('/user/update', { data })
+  },
+  getInfo() {
+    const token = JSON.parse(localStorage.getItem('token'))
+    if (token) {
+      return api.get('/user/get-info', {
+        headers: {
+          Authorization: `Bearer ${token.accessToken}`
+        }
+      })
+    }
+  }
 }
 
 export default userService
